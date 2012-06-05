3ab64c563086252127e7bfca2dfe284dfe30d136
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-05 14:55:50 +0100

    Display banners on the home page.

--- kanisa/views.py
+++ kanisa/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from kanisa.models.banners import Banner
+
 
 def index(request):
+    banners = Banner.objects.all()
+
     return render_to_response('kanisa/index.html',
-                              {},
+                              {'banners': banners},
                               context_instance=RequestContext(request))
