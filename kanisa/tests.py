af2758632598e40dddef9a9e84fcd3de7be5f929
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-05 17:14:44 +0100

    Adding basic set of tests.

--- kanisa/tests.py
+++ kanisa/tests.py
@@ -1,16 +1,20 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.core.files.storage import default_storage
 from django.test import TestCase
+from kanisa.models import Banner
+import os
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class BannerTest(TestCase):
+    fixtures = ['banners.json', ]
+
+    def setUp(self):
+        media_dir = os.path.join(os.path.dirname(__file__), 'fixtures/media')
+        self._old_default_storage_location = default_storage.location
+        default_storage.location = media_dir
+
+    def tearDown(self):
+        default_storage.location = self._old_default_storage_location
+
+    def testBasics(self):
+        banners = Banner.objects.all()
+        self.assertEqual(len(banners), 3)
