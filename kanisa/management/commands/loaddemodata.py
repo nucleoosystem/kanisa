019682c4e2b08419bd41adfaab56a83e65afb42e
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-06 23:04:40 +0100

    Adding a quick way to load sample data for demo purposes.
    Note that this includes a rather horrible hack where I duplicate
    uploaded file paths, though this is probably trivially fixed by
    just removing the file path from the fixture itself.

---
+++ kanisa/management/commands/loaddemodata.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
+import kanisa.models
+import os, os.path
+import shutil
+
+class Command(BaseCommand):
+    help = 'Loads sample data for demonstrating Kanisa'
+
+    def handle(self, *args, **options):
+        if not getattr(settings, 'MEDIA_ROOT', None):
+            raise CommandError("Please define MEDIA_ROOT.")
+
+        self.load_fixtures()
+        self.copy_media()
+
+    def load_fixtures(self):
+        call_command('loaddata', 'banners.json')
+
+    def copy_media(self):
+        kanisa_directory = os.path.dirname(kanisa.models.__file__)
+        kanisa_fixtures_media = os.path.join(kanisa_directory, '../', 'fixtures/media')
+
+        src_files = os.listdir(kanisa_fixtures_media)
+
+        for file_name in src_files:
+            full_file_name = os.path.join(kanisa_fixtures_media, file_name)
+            if (os.path.isfile(full_file_name)):
+                shutil.copy(full_file_name,
+                            os.path.join(settings.MEDIA_ROOT,
+                                         "kanisa/banners",
+                                         file_name))
