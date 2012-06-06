22d0cb14fcebc581336a7e9e150b272f8b6eb8c7
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-06 22:42:13 +0100

    Up to 95% coverage of banners.py now.

--- kanisa/tests.py
+++ kanisa/tests.py
@@ -16,16 +16,36 @@ class BannerTest(TestCase):
         default_storage.location = self._old_default_storage_location
 
     def testIsActive(self):
+        # A banner with no expiry date or publication date
         banner = Banner.objects.get(pk=1)
         self.assertTrue(banner.active())
 
+        # A banner which has expired
+        banner = Banner.objects.get(pk=4)
+        self.assertFalse(banner.active())
+
+        # A banner with a long-passed publication date, and a
+        # far-future expiration date
+        banner = Banner.objects.get(pk=5)
+        self.assertFalse(banner.expired())
+
     def testHasExpired(self):
+        # A banner with no expiry date or publication date
         banner = Banner.objects.get(pk=1)
-        self.assertFalse(banner.expired())        
+        self.assertFalse(banner.expired())
+
+        # A banner which has expired
+        banner = Banner.objects.get(pk=4)
+        self.assertTrue(banner.expired())
+
+        # A banner with a long-passed publication date, and a
+        # far-future expiration date
+        banner = Banner.objects.get(pk=5)
+        self.assertFalse(banner.expired())
 
     def testFetchActive(self):
         banners = Banner.active_objects.all()
-        self.assertEqual(len(banners), 3)
+        self.assertEqual(len(banners), 4)
 
     def testUnicode(self):
         banner = Banner.objects.get(pk=1)
