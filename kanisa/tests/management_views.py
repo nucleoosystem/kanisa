ff96391e4415d3a78acccae82dad2fa2a1949bca
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-08 19:17:31 +0100

    Adding some basic tests for the banner management view.

--- kanisa/tests/management_views.py
+++ kanisa/tests/management_views.py
@@ -5,6 +5,8 @@ from django.test import TestCase
 
 
 class ManagementViewTests(TestCase):
+    fixtures = ['banners.json', ]
+
     def setUp(self):
         bob = User.objects.create_user('bob', '', 'secret')
         bob.is_staff = False
@@ -14,28 +16,38 @@ class ManagementViewTests(TestCase):
         fred.is_staff = True
         fred.save()
 
-    def test_management_index_protected(self):
-        url = reverse('kanisa.views.manage')
+    def test_views_protected(self):
+        self.__test_staff_only('kanisa.views.manage',
+                               'kanisa/management/index.html')
+        self.__test_staff_only('kanisa.views.manage_banners',
+                               'kanisa/management/banners/index.html')
+
+    def test_banner_management_view(self):
+        url = reverse('kanisa.views.manage_banners')
+        self.client.login(username='fred', password='secret')
+        resp = self.client.get(url)
+        self.assertTrue('banners' in resp.context)
+        self.assertEqual([banner.pk for banner in resp.context['banners']],
+                         [1, 2, 3, 5, ])
+
+    def __test_staff_only(self, view_name, success_template):
+        # Not logged in
+        url = reverse(view_name)
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'admin/login.html')
 
-    def test_management_index_disallows_non_staff(self):
-        url = reverse('kanisa.views.manage')
+        # Logged in as non-staff member
         self.client.login(username='bob', password='secret')
-
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'admin/login.html')
-
         self.client.logout()
 
-    def test_management_index_allows_staff_in(self):
-        url = reverse('kanisa.views.manage')
+        # Logged in as staff member
         self.client.login(username='fred', password='secret')
-
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'kanisa/management/index.html')
+        self.assertTemplateUsed(resp, success_template)
 
         self.client.logout()
