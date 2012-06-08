9ca006b01b893e67c3c4bc5f7440f752e8d9f94d
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-08 07:42:17 +0100

    Adding example_project, to make it easy to run test cases.

---
+++ kanisa/example_project/settings.py
@@ -0,0 +1,46 @@
+import os
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+SITE_ID = 1
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+MEDIA_URL = STATIC_URL + "media/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
+TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
+SECRET_KEY = "incrediblysecretkeyyoushouldchange"
+ADMINS = ()
+MANAGERS = ADMINS
+LOGIN_URL = "/admin/"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'dev.db',
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
+    'sorl.thumbnail',
+    'kanisa',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'kanisa.context_processors.kanisa_settings',
+)
