dc8bb3c4bb08bc4d66f0e196d16938a8036428ee
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-09 09:41:18 +0100

    Don't test management views visible to staff in __test_staff_only.
    Test that in a test dedicated to that view, which ensures it is
    rendered correctly.

--- kanisa/tests/management_views.py
+++ kanisa/tests/management_views.py
@@ -17,20 +17,28 @@ class ManagementViewTests(TestCase):
         fred.save()
 
     def test_views_protected(self):
-        self.__test_staff_only('kanisa.views.manage',
-                               'kanisa/management/index.html')
-        self.__test_staff_only('kanisa.views.manage_banners',
-                               'kanisa/management/banners/index.html')
+        self.__test_staff_only('kanisa.views.manage')
+        self.__test_staff_only('kanisa.views.manage_banners')
+
+    def test_root_view(self):
+        url = reverse('kanisa.views.manage')
+        self.client.login(username='fred', password='secret')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'kanisa/management/index.html')
 
     def test_banner_management_view(self):
         url = reverse('kanisa.views.manage_banners')
         self.client.login(username='fred', password='secret')
         resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'kanisa/management/banners/index.html')
+
         self.assertTrue('banners' in resp.context)
         self.assertEqual([banner.pk for banner in resp.context['banners']],
                          [1, 2, 3, 5, ])
 
-    def __test_staff_only(self, view_name, success_template):
+    def __test_staff_only(self, view_name):
         # Not logged in
         url = reverse(view_name)
         resp = self.client.get(url)
@@ -42,12 +50,4 @@ class ManagementViewTests(TestCase):
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'admin/login.html')
-        self.client.logout()
-
-        # Logged in as staff member
-        self.client.login(username='fred', password='secret')
-        resp = self.client.get(url)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, success_template)
-
         self.client.logout()
