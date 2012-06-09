e75981bccde5f46098368679a2b739e151655f26
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-09 19:22:10 +0100

    Adding a BaseForm for crispy forms stuff.

--- kanisa/forms.py
+++ kanisa/forms.py
@@ -5,16 +5,20 @@ from crispy_forms.layout import Submit
 from kanisa.models.banners import Banner
 
 
-class BannerForm(ModelForm):
-    class Meta:
-        model = Banner
-
+class BaseForm(ModelForm):
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
 
         css = "btn-primary btn-large btn-success"
-        self.helper.add_input(Submit('submit', 'Save Banner',
+        submit_text = 'Save %s' % self._meta.model._meta.verbose_name.title()
+        self.helper.add_input(Submit('submit',
+                                     submit_text,
                                      css_class=css))
         self.helper.form_class = 'form-horizontal'
 
-        super(BannerForm, self).__init__(*args, **kwargs)
+        super(BaseForm, self).__init__(*args, **kwargs)
+
+
+class BannerForm(BaseForm):
+    class Meta:
+        model = Banner
