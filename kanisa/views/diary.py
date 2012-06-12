b13043a37b39160a16f2a90537175ffc86463b73
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-12 18:07:28 +0100

    Add a view of upcoming events.
    This doesn't actually show any upcoming events - it's really just a list
    of regularly scheduled events as it'd look in a week-to-view diary.
    Eventually each event will have a little "Schedule" button next to it,
    to add an occurrence of the event for the current week (or future
    weeks), and the reverse for removing an occurrence of an event.

--- kanisa/views/diary.py
+++ kanisa/views/diary.py
@@ -3,7 +3,8 @@ from django.core.urlresolvers import reverse
 
 
 from kanisa.forms import RegularEventForm
-from kanisa.models import RegularEvent
+from kanisa.models import RegularEvent, DiaryEventOccurrence
+from kanisa.utils import get_week_bounds
 from kanisa.views.generic import (KanisaCreateView, KanisaUpdateView,
                                   KanisaListView, KanisaTemplateView)
 
@@ -21,6 +22,31 @@ class DiaryEventIndexView(KanisaTemplateView, DiaryBaseView):
     template_name = 'kanisa/management/diary/index.html'
     kanisa_title = 'Manage Diary'
     kanisa_is_root_view = True
+
+    def get_context_data(self, **kwargs):
+        context = super(DiaryEventIndexView,
+                        self).get_context_data(**kwargs)
+
+        # Get events for this week, along with all regular events
+        regular_events = RegularEvent.objects.all()
+        monday, sunday = get_week_bounds()
+        scheduled_events = DiaryEventOccurrence.objects.\
+                            exclude(date__lt=monday,
+                                    date__gt=sunday)
+
+        calendar_entries = []
+
+        from kanisa.models import diary
+
+        for i in range(0, 7):
+            calendar_entries.append((diary.DAYS_OF_WEEK[i][1], []))
+
+        for event in regular_events:
+            calendar_entries[event.day][1].append(event)
+
+        context['calendar'] = calendar_entries
+
+        return context
 
 
 class DiaryRegularEventsView(KanisaListView, DiaryBaseView):
