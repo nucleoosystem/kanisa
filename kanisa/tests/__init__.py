daee5dd31b7f7c747e2b28d8ec305121ec990967
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-13 22:53:37 +0100

    Missed off a test.

--- kanisa/tests/__init__.py
+++ kanisa/tests/__init__.py
@@ -1,4 +1,5 @@
 from kanisa.tests.banners import BannerTest
-from kanisa.tests.diary import DiaryTest, DiaryGetWeekBoundsTest
+from kanisa.tests.diary import (DiaryTest, DiaryGetWeekBoundsTest,
+                                DiaryGetScheduleTest)
 from kanisa.tests.management_views import ManagementViewTests
 from kanisa.tests.public_views import PublicViewTests
