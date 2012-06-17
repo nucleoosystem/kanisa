c1d7929d7bbe21b1af3ddddc2b34eab71e392c94
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-17 08:21:43 +0100

    Add a quick test for the diary index view.

--- kanisa/tests/management_views.py
+++ kanisa/tests/management_views.py
@@ -3,7 +3,7 @@ from kanisa.tests.utils import KanisaViewTestCase
 
 
 class ManagementViewTests(KanisaViewTestCase):
-    fixtures = ['banners.json', ]
+    fixtures = ['banners.json', 'diary.json', ]
 
     def test_views_protected(self):
         self.check_staff_only(reverse('kanisa.views.manage'))
@@ -57,3 +57,14 @@ class ManagementViewTests(KanisaViewTestCase):
                              'This field is required.')
         self.assertFormError(resp, 'form', 'image',
                              'This field is required.')
+
+    def test_diary_root_view(self):
+        url = reverse('kanisa_manage_diary')
+        self.client.login(username='fred', password='secret')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'kanisa/management/diary/index.html')
+        self.assertTrue('calendar' in resp.context)
+        self.assertTrue('events_to_schedule' in resp.context)
+        self.assertTrue(resp.context['events_to_schedule'])
+        self.assertEqual(len(resp.context['calendar']), 7)
