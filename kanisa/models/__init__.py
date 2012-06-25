5b65b24ff841948228c28e7fb530f601cb742103
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-25 17:35:24 +0100

    Forgot to add SermonSpeak to __init__.py

--- kanisa/models/__init__.py
+++ kanisa/models/__init__.py
@@ -1,4 +1,4 @@
 from kanisa.models.banners import Banner
 from kanisa.models.diary import RegularEvent, ScheduledEvent
-from kanisa.models.sermons import SermonSeries, Sermon
+from kanisa.models.sermons import SermonSeries, Sermon, SermonSpeaker
 from kanisa.models.documents import Document
