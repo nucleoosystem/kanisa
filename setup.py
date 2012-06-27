7a0d04f2cd156f5f03f42cfe9c72e440f6cd13ff
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-27 06:41:01 +0100

    Sorting requirements files.

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ setup(
     include_package_data=True,
     install_requires=[
         "django",
-        "sorl-thumbnail==11.12",
         "django-crispy-forms==1.1.4",
         "django-haystack==1.2.7",
         "mutagen==1.20",
+        "sorl-thumbnail==11.12",
         "Whoosh==2.4.1",
     ],
 )
