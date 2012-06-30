eaf45890756b7b73248321530174438954a3eed5
Dominic Rodger <dominicrodger+github@gmail.com>
2012-06-30 10:36:08 +0100

    Adding facility for searching for banners.

--- kanisa/search_indexes.py
+++ kanisa/search_indexes.py
@@ -1,7 +1,7 @@
 import datetime
 from haystack import indexes
 from haystack import site
-from kanisa.models import SermonSeries, Document
+from kanisa.models import SermonSeries, Document, Banner
 from sorl.thumbnail import get_thumbnail
 
 
@@ -33,3 +33,21 @@ class SermonSeriesIndex(indexes.SearchIndex):
         return im.name
 
 site.register(SermonSeries, SermonSeriesIndex)
+
+
+class BannerIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+    image = indexes.CharField(model_attr='image')
+    title = indexes.CharField(model_attr='headline')
+    details = indexes.CharField(model_attr='contents', null=True)
+    url = indexes.CharField(model_attr='url')
+
+    def index_queryset(self):
+        """Used when the entire index for model is updated."""
+        return Banner.objects.all()
+
+    def prepare_image(self, obj):
+        im = get_thumbnail(obj.image, '100x100', crop='center')
+        return im.name
+
+site.register(Banner, BannerIndex)
