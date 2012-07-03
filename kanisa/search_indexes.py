8d37257d7f082345f9d7bd1d2974b51a8bc658e3
Dominic Rodger <dominicrodger+github@gmail.com>
2012-07-03 18:15:28 +0100

    Don't need custom SearchIndexes now, mostly.
    Only exception is BannerIndex, since I used a different name for the
    "title" field.

--- kanisa/search_indexes.py
+++ kanisa/search_indexes.py
@@ -12,39 +12,19 @@ class KanisaBaseSearchIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     modified = indexes.DateTimeField(model_attr='modified')
     rendered = indexes.CharField(use_template=True)
+    title = indexes.CharField(model_attr='title')
 
     def get_updated_field(self):
         return 'modified'
 
 
-class DocumentIndex(KanisaBaseSearchIndex):
-    title = indexes.CharField(model_attr='title')
-site.register(Document, DocumentIndex)
-
-
-class SermonSeriesIndex(KanisaBaseSearchIndex):
-    title = indexes.CharField(model_attr='title')
-site.register(SermonSeries, SermonSeriesIndex)
-
-
-class SermonIndex(KanisaBaseSearchIndex):
-    title = indexes.CharField(model_attr='title')
-site.register(Sermon, SermonIndex)
-
+site.register(Document, KanisaBaseSearchIndex)
+site.register(SermonSeries, KanisaBaseSearchIndex)
+site.register(Sermon, KanisaBaseSearchIndex)
+site.register(RegularEvent, KanisaBaseSearchIndex)
+site.register(ScheduledEvent, KanisaBaseSearchIndex)
 
 class BannerIndex(KanisaBaseSearchIndex):
     title = indexes.CharField(model_attr='headline')
 
 site.register(Banner, BannerIndex)
-
-
-class RegularEventIndex(KanisaBaseSearchIndex):
-    title = indexes.CharField(model_attr='title')
-
-site.register(RegularEvent, RegularEventIndex)
-
-
-class ScheduledEventIndex(KanisaBaseSearchIndex):
-    title = indexes.CharField(model_attr='title')
-
-site.register(ScheduledEvent, ScheduledEventIndex)
