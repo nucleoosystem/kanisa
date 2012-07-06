1c615fcfe263e1ae8edf16d69f524778fcce8ada
Dominic Rodger <dominicrodger+github@gmail.com>
2012-07-06 08:11:16 +0100

    No more custom attributes on searches.
    The only reason custom attributes were needed was to avoid database
    queries for objects, and to customise the way search results were
    displayed. All of that is now more elegantly handled by having base
    search templates, and then overriding the bits that are needed (the only
    reason I needed the custom search index objects was for modifying
    titles/subtitles, which are more elegantly handled by blocks in
    templates).

--- kanisa/search_indexes.py
+++ kanisa/search_indexes.py
@@ -1,4 +1,3 @@
-from django.utils.dateformat import DateFormat
 from haystack import indexes
 from haystack import site
 from kanisa.models import (Sermon, SermonSeries, SermonSpeaker,
@@ -10,7 +9,6 @@ from kanisa.models import (Sermon, SermonSeries, SermonSpeaker,
 class KanisaBaseSearchIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     rendered = indexes.CharField(use_template=True)
-    title = indexes.CharField(model_attr='title')
 
     def get_updated_field(self):
         return 'modified'
@@ -20,28 +18,6 @@ site.register(Document, KanisaBaseSearchIndex)
 site.register(SermonSeries, KanisaBaseSearchIndex)
 site.register(Sermon, KanisaBaseSearchIndex)
 site.register(RegularEvent, KanisaBaseSearchIndex)
-
-
-class ScheduledEventIndex(KanisaBaseSearchIndex):
-    subtitle = indexes.CharField(model_attr='title')
-
-    def prepare_subtitle(self, obj):
-        thedate = DateFormat(obj.date).format('l, jS F Y')
-        return '%s' % thedate
-
-site.register(ScheduledEvent, ScheduledEventIndex)
-
-
-class BannerIndex(KanisaBaseSearchIndex):
-    title = indexes.CharField(model_attr='headline')
-
-site.register(Banner, BannerIndex)
-
-
-class SermonSpeakerIndex(KanisaBaseSearchIndex):
-    title = indexes.CharField()
-
-    def prepare_title(self, obj):
-        return '%s %s' % (obj.forename, obj.surname)
-
-site.register(SermonSpeaker, SermonSpeakerIndex)
+site.register(ScheduledEvent, KanisaBaseSearchIndex)
+site.register(Banner, KanisaBaseSearchIndex)
+site.register(SermonSpeaker, KanisaBaseSearchIndex)
