6787f4718ca885fa923da5618d8f33f689d2778f
Dominic Rodger <dominicrodger+github@gmail.com>
2012-07-12 08:21:55 +0100

    Include number of scheduled tweets in the sidebar.

--- kanisa/models/social.py
+++ kanisa/models/social.py
@@ -1,8 +1,18 @@
 from __future__ import absolute_import
 
+from datetime import date, datetime
 from django.db import models
 
 from .base import SearchableModel
+
+
+class FutureScheduledTweetsManager(models.Manager):
+    def get_query_set(self):
+        now = datetime.now()
+        qs = super(FutureScheduledTweetsManager, self).get_query_set()
+        qs = qs.exclude(date__lt=now.date())
+        qs = qs.exclude(date=now.date(), time__lt=now.time())
+        return qs
 
 
 class ScheduledTweet(SearchableModel):
@@ -14,6 +24,10 @@ class ScheduledTweet(SearchableModel):
                                             'been posted to Twitter.'))
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    # Managers
+    objects = models.Manager()
+    future_objects = FutureScheduledTweetsManager()
 
     class Meta:
         # Need this because I've split up models.py into multiple
--- kanisa/templatetags/kanisa_tags.py
+++ kanisa/templatetags/kanisa_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from kanisa.models import Banner
+from kanisa.models import Banner, ScheduledTweet
 
 
 register = template.Library()
@@ -8,3 +8,8 @@ register = template.Library()
 @register.simple_tag()
 def kanisa_active_banners():
     return Banner.active_objects.count()
+
+
+@register.simple_tag()
+def kanisa_future_scheduled_tweets():
+    return ScheduledTweet.future_objects.count()
