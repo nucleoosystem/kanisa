6c03850b0ba2662345206677db91331e582e6aef
Dominic Rodger <dominicrodger+github@gmail.com>
2012-07-13 08:16:09 +0100

    Renaming document search view for consistency.
    This brings this view into line with all the other document views.

--- kanisa/tests/document_views.py
+++ kanisa/tests/document_views.py
@@ -6,7 +6,7 @@ class DocumentManagementViewTest(KanisaViewTestCase):
     def test_views_protected(self):
         self.check_staff_only(reverse('kanisa_manage_documents'))
         self.check_staff_only(reverse('kanisa_manage_documents_create'))
-        self.check_staff_only(reverse('kanisa_search_documents'))
+        self.check_staff_only(reverse('kanisa_manage_documents_search'))
 
         # These would 404 if you were logged in
         self.check_staff_only(reverse('kanisa_manage_documents_update', args=[1, ]))
