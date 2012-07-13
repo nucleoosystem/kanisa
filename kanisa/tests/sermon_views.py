94666db67db44f45c8a73e978f16ee80dc15c568
Dominic Rodger <dominicrodger+github@gmail.com>
2012-07-13 22:10:28 +0100

    More tests for sermon views.

--- kanisa/tests/sermon_views.py
+++ kanisa/tests/sermon_views.py
@@ -3,6 +3,8 @@ from kanisa.tests.utils import KanisaViewTestCase
 
 
 class SermonManagementViewTest(KanisaViewTestCase):
+    fixtures = ['sermons.json', ]
+
     def test_views_protected(self):
         prefix = 'kanisa_manage_sermons'
         self.check_staff_only_302(reverse(prefix))
@@ -11,7 +13,6 @@ class SermonManagementViewTest(KanisaViewTestCase):
         self.check_staff_only_302(reverse('%s_speaker' % prefix))
         self.check_staff_only_302(reverse('%s_speaker_create' % prefix))
 
-        # These would 404 if you were logged in
         self.check_staff_only_302(reverse('%s_series_detail' % prefix,
                                           args=[1, ]))
         self.check_staff_only_302(reverse('%s_series_update' % prefix,
@@ -22,3 +23,57 @@ class SermonManagementViewTest(KanisaViewTestCase):
                                           args=[1, ]))
         self.check_staff_only_302(reverse('%s_speaker_update' % prefix,
                                           args=[1, ]))
+
+    def test_index_view(self):
+        self.client.login(username='fred', password='secret')
+        resp = self.client.get(reverse('kanisa_manage_sermons'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'kanisa/management/sermons/index.html')
+
+        self.assertEqual(len(resp.context['standalone']), 1)
+        self.assertEqual(len(resp.context['object_list']), 3)
+        self.client.logout()
+
+    def test_create_series_view(self):
+        self.client.login(username='fred', password='secret')
+
+        resp = self.client.get(reverse('kanisa_manage_sermons_series_create'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'kanisa/management/create.html')
+
+        self.client.logout()
+
+    def test_create_series_view(self):
+        self.client.login(username='fred', password='secret')
+        base_url = reverse('kanisa_manage_sermons_individual_create')
+
+        # Check with a pre-populated series
+        url = '%s?series=2' % base_url
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'kanisa/management/create.html')
+        self.assertEqual(resp.context['form'].initial['series'], '2')
+
+        # Check without a pre-populated series
+        resp = self.client.get(base_url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'kanisa/management/create.html')
+        self.assertTrue('series' not in resp.context['form'].initial)
+
+        # Check with an invalid pre-populated series
+        url = '%s?series=foobar' % base_url
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'kanisa/management/create.html')
+        self.assertEqual(resp.context['form'].initial['series'], 'foobar')
+
+        self.client.logout()
+
+    def test_create_speaker_view(self):
+        self.client.login(username='fred', password='secret')
+
+        resp = self.client.get(reverse('kanisa_manage_sermons_speaker_create'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'kanisa/management/create.html')
+
+        self.client.logout()
