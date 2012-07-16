ba841ccd6be1ceeab43a4d41387b862a2a6f350c
Dominic Rodger <dominicrodger+github@gmail.com>
2012-07-16 08:10:32 +0100

    Removing now unused function.

--- kanisa/tests/utils.py
+++ kanisa/tests/utils.py
@@ -14,19 +14,6 @@ class KanisaViewTestCase(TestCase):
         fred.user_permissions.add(p)
         fred.save()
 
-    def check_staff_only(self, url):
-        # Not logged in
-        resp = self.client.get(url)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'admin/login.html')
-
-        # Logged in as non-staff member
-        self.client.login(username='bob', password='secret')
-        resp = self.client.get(url)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'admin/login.html')
-        self.client.logout()
-
     def check_staff_only_302(self, url):
         # Not logged in
         resp = self.client.get(url)
