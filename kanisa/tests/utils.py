c2f3096116b853573939df2b6154bcb0bf689429
Dominic Rodger <dominicrodger+github@gmail.com>
2012-07-16 22:02:58 +0100

    Switch to assertRedirects instead of checking redirect_chain.

--- kanisa/tests/utils.py
+++ kanisa/tests/utils.py
@@ -19,14 +19,17 @@ class KanisaViewTestCase(TestCase):
         fred.user_permissions.add(p)
         fred.save()
 
-    @override_settings(LOGIN_URL='/accounts/login/')
+    @override_settings(LOGIN_URL='/loginview/')
     def view_is_restricted(self, url):
         # Not logged in
         resp = self.client.get(url)
-        self.assertEqual(resp.status_code, 302)
-        expected = 'http://testserver/accounts/login/?next=%s' % url
-        self.assertEqual(resp.items()[-1],
-                         ('Location', expected))
+
+        # The loginview URL does not exist. I should probably make
+        # sure it does, but for now I'll just say this should 404, and
+        # set the URL to something that probably won't exist. This
+        # could do with tidying up.
+        self.assertRedirects(resp, '/loginview/?next=%s' % url,
+                             302, 404)
 
         # Logged in as non-staff member
         self.client.login(username='bob', password='secret')
