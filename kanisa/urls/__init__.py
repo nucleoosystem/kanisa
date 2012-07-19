dfa09c55b8a70ec92757a126f9074612467f021e
Dominic Rodger <dominicrodger+github@gmail.com>
2012-07-19 07:55:12 +0100

    Separate off URLs starting with "manage/".

--- kanisa/urls/__init__.py
+++ kanisa/urls/__init__.py
@@ -1,6 +1,4 @@
 from django.conf.urls import patterns, include, url
-from kanisa.views import (KanisaManagementIndexView,
-                          KanisaSearchView)
 from kanisa.views.banners import VisitBannerView
 
 
@@ -10,24 +8,6 @@ urlpatterns = patterns('',
                            VisitBannerView.as_view(),
                            {},
                            'kanisa_public_banners_visit'),
-                       url(r'^manage/$',
-                           KanisaManagementIndexView.as_view(),
-                           {},
-                           'kanisa_manage_index'),
-                       url(r'^manage/search/$',
-                           KanisaSearchView.as_view(),
-                           {},
-                           'kanisa_manage_search'),
-                       url(r'^manage/banners/',
-                           include('kanisa.urls.banners')),
-                       url(r'^manage/diary/',
-                           include('kanisa.urls.diary')),
-                       url(r'^manage/sermons/',
-                           include('kanisa.urls.sermons')),
-                       url(r'^manage/documents/',
-                           include('kanisa.urls.documents')),
-                       url(r'^manage/social/',
-                           include('kanisa.urls.social')),
-                       url(r'^manage/xhr/',
-                           include('kanisa.urls.xhr')),
+                       url(r'^manage/',
+                           include('kanisa.urls.management')),
                        )
---
+++ kanisa/urls/management.py
@@ -0,0 +1,27 @@
+from django.conf.urls import patterns, include, url
+from kanisa.views import (KanisaManagementIndexView,
+                          KanisaSearchView)
+
+
+urlpatterns = patterns('',
+                       url(r'^$',
+                           KanisaManagementIndexView.as_view(),
+                           {},
+                           'kanisa_manage_index'),
+                       url(r'^search/$',
+                           KanisaSearchView.as_view(),
+                           {},
+                           'kanisa_manage_search'),
+                       url(r'^banners/',
+                           include('kanisa.urls.banners')),
+                       url(r'^diary/',
+                           include('kanisa.urls.diary')),
+                       url(r'^sermons/',
+                           include('kanisa.urls.sermons')),
+                       url(r'^documents/',
+                           include('kanisa.urls.documents')),
+                       url(r'^social/',
+                           include('kanisa.urls.social')),
+                       url(r'^xhr/',
+                           include('kanisa.urls.xhr')),
+                       )
