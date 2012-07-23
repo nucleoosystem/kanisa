ff2a5a818adba68d563786cfae9bbc241c9d956b
Dominic Rodger <dominicrodger+github@gmail.com>
2012-07-23 17:48:11 +0100

    Show which permissions users have (currently probably O(n)).
    Also needs some styling love (center ticks within columns).

--- kanisa/templatetags/kanisa_tags.py
+++ kanisa/templatetags/kanisa_tags.py
@@ -13,3 +13,11 @@ def kanisa_active_banners():
 @register.simple_tag()
 def kanisa_future_scheduled_tweets():
     return ScheduledTweet.future_objects.count()
+
+
+@register.simple_tag(takes_context=True)
+def kanisa_user_has_perm(context, perm):
+    user = context['user']
+    if user.has_perm(perm):
+        return '<i class="icon icon-ok"></i>'
+    return ''
