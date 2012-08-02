2206046ad38eac0f3dc36e30d05c0b11b31b3863
Dominic Rodger <dominicrodger+github@gmail.com>
2012-08-02 13:09:02 +0300

    Switch to EpicEditor for scheduled event editing.

--- kanisa/forms.py
+++ kanisa/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms import ModelForm
 from django.forms.util import ErrorList
+from django.template.loader import render_to_string
+from django.utils.encoding import force_unicode
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
@@ -106,6 +110,30 @@ class RegularEventForm(KanisaBaseForm):
         model = RegularEvent
 
 
+class EpicWidget(forms.Textarea):
+    class Media:
+        js = ('kanisa/epiceditor/js/epiceditor.js', )
+
+    def __init__(self, *args, **kwargs):
+        if 'attrs' not in kwargs:
+            kwargs['attrs'] = {}
+
+        super(EpicWidget, self).__init__(*args, **kwargs)
+
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+
+        final_attrs = self.build_attrs(attrs, name=name)
+
+        escaped = conditional_escape(force_unicode(value))
+        output = render_to_string('kanisa/management/_epic_widget.html',
+                                  {'id': final_attrs["id"],
+                                   'name': final_attrs["name"],
+                                   'value': escaped, })
+        return mark_safe(output)
+
+
 class ScheduledEventEditForm(KanisaBaseForm):
     start_time = BootstrapTimeField()
     date = BootstrapDateField()
@@ -113,6 +141,7 @@ class ScheduledEventEditForm(KanisaBaseForm):
     class Meta:
         model = ScheduledEvent
         exclude = ('event', )
+        widgets = {'details': EpicWidget(), }
 
 
 class ScheduledEventCreationForm(KanisaBaseForm):
