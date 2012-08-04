f1467aa2d9e2da0ee930530dba458bb2ae4ba808
Dominic Rodger <dominicrodger+github@gmail.com>
2012-08-04 22:22:32 +0300

    Show page hierarchy in the dropdown list.

--- kanisa/templatetags/kanisa_tags.py
+++ kanisa/templatetags/kanisa_tags.py
@@ -45,3 +45,27 @@ def kanisa_user_has_perm(context, perm):
     html_attr = ['%s="%s"' % (k, v) for k, v in attributes.items()]
 
     return input % ' '.join(html_attr)
+
+
+@register.filter
+def get_range(value):
+    """
+    From http://djangosnippets.org/snippets/1357/
+
+    Filter - returns a list containing range made from given value
+    Usage (in template):
+
+    <ul>{% for i in 3|get_range %}
+    <li>{{ i }}. Do something</li>
+    {% endfor %}</ul>
+
+    Results with the HTML:
+    <ul>
+    <li>0. Do something</li>
+    <li>1. Do something</li>
+    <li>2. Do something</li>
+    </ul>
+
+    Instead of 3 one may use the variable set in the views
+    """
+    return range(value)
