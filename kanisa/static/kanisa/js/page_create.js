8a885ae33fad6562e68f4c0d7d84794e5653fc45
Dominic Rodger <dominicrodger+github@gmail.com>
2012-08-04 18:40:24 +0300

    Horrible first attempt at preventing multiple submissions.

--- kanisa/static/kanisa/js/page_create.js
+++ kanisa/static/kanisa/js/page_create.js
@@ -5,13 +5,22 @@ function update_page_list() {
           });
 }
 
+function do_nothing() {
+    return false;
+}
+
 function quick_page_create() {
     var form = $(this);
+    form.unbind('submit', quick_page_create);
+    form.bind('submit', do_nothing);
+
     var button = form.find("button");
     button.attr("disabled", "disabled");
+
     var title_element = form.find("#id_title");
     var title = title_element.val();
     var status_block = form.find("div");
+
     status_block.html("<i class=\"spinner\"></i>");
 
     $.post(page_create_url,
@@ -22,9 +31,13 @@ function quick_page_create() {
                title_element.val("");
                update_page_list();
                button.removeAttr("disabled");
+               form.bind('submit', quick_page_create);
+               form.unbind('submit', do_nothing);
            }).error(function(data) {
                status_block.html("<i class=\"icon-exclamation-sign\"></i> " + data.responseText);
                button.removeAttr("disabled");
+               form.bind('submit', quick_page_create);
+               form.unbind('submit', do_nothing);
            });
 
     return false;
