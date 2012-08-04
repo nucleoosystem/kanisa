417a665c8f989f53b53badc3e16332ad44f2247a
Dominic Rodger <dominicrodger+github@gmail.com>
2012-08-04 17:20:53 +0300

    Re-indenting.

--- kanisa/static/kanisa/js/page_create.js
+++ kanisa/static/kanisa/js/page_create.js
@@ -1,21 +1,21 @@
 $(function() {
-  $("#page_quick_create").submit(function() {
-    var form = $(this);
-    var title_element = form.find("#id_title");
-    var title = title_element.val();
-    var status_block = form.find("div");
-    status_block.html("<i class=\"spinner\"></i>");
+    $("#page_quick_create").submit(function() {
+        var form = $(this);
+        var title_element = form.find("#id_title");
+        var title = title_element.val();
+        var status_block = form.find("div");
+        status_block.html("<i class=\"spinner\"></i>");
 
-    $.post(page_create_url,
-           { 'title': title,
-             'parent': '' },
-           function(data) {
-             status_block.html("<i class=\"icon-ok\"></i> " + data);
-             title_element.val("");
-           }).error(function(data) {
-             status_block.html("<i class=\"icon-exclamation-sign\"></i> " + data.responseText);
-           });
+        $.post(page_create_url,
+               { 'title': title,
+                 'parent': '' },
+               function(data) {
+                   status_block.html("<i class=\"icon-ok\"></i> " + data);
+                   title_element.val("");
+               }).error(function(data) {
+                   status_block.html("<i class=\"icon-exclamation-sign\"></i> " + data.responseText);
+               });
 
-    return false;
-  });
+        return false;
+    });
 });
