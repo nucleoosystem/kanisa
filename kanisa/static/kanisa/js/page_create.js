9966a774e126cb3dc741ebe08b503dcb2fb3169b
Dominic Rodger <dominicrodger+github@gmail.com>
2012-08-04 18:31:52 +0300

    Re-indenting.

--- kanisa/static/kanisa/js/page_create.js
+++ kanisa/static/kanisa/js/page_create.js
@@ -6,29 +6,29 @@ function update_page_list() {
 }
 
 function quick_page_create() {
-        var form = $(this);
-        var button = form.find("button");
-        button.attr("disabled", "disabled");
-        var title_element = form.find("#id_title");
-        var title = title_element.val();
-        var status_block = form.find("div");
-        status_block.html("<i class=\"spinner\"></i>");
+    var form = $(this);
+    var button = form.find("button");
+    button.attr("disabled", "disabled");
+    var title_element = form.find("#id_title");
+    var title = title_element.val();
+    var status_block = form.find("div");
+    status_block.html("<i class=\"spinner\"></i>");
 
-        $.post(page_create_url,
-               { 'title': title,
-                 'parent': '' },
-               function(data) {
-                   status_block.html("<i class=\"icon-ok\"></i> " + data);
-                   title_element.val("");
-                   update_page_list();
-                   button.removeAttr("disabled");
-               }).error(function(data) {
-                   status_block.html("<i class=\"icon-exclamation-sign\"></i> " + data.responseText);
-                   button.removeAttr("disabled");
-               });
+    $.post(page_create_url,
+           { 'title': title,
+             'parent': '' },
+           function(data) {
+               status_block.html("<i class=\"icon-ok\"></i> " + data);
+               title_element.val("");
+               update_page_list();
+               button.removeAttr("disabled");
+           }).error(function(data) {
+               status_block.html("<i class=\"icon-exclamation-sign\"></i> " + data.responseText);
+               button.removeAttr("disabled");
+           });
 
-        return false;
-    }
+    return false;
+}
 
 $(function() {
     $("#page_quick_create").submit(quick_page_create);
