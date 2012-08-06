060bd3dcb70208d2279f0903d3b473d14cad0a81
Dominic Rodger <dominicrodger+github@gmail.com>
2012-08-06 14:13:49 +0300

    Allow marking sermon series as complete via XHR.

---
+++ kanisa/static/kanisa/js/kanisa_sermons.js
@@ -0,0 +1,23 @@
+function quick_series_complete(event) {
+    event.preventDefault();
+    var el = $(this);
+    el.hide();
+    var series = el.attr("data-series-id");
+    var status_block = el.siblings("span");
+
+    status_block.html("<i class=\"spinner\"></i>");
+
+    $.post(series_complete_url, {'series': series},
+           function(data) {
+               el.remove();
+               status_block.html('<i class="icon-ok"></i> ' + data);
+           }).error(function(data) {
+               alert_failure(data.responseText);
+               el.show();
+               status_block.html('');
+           });
+}
+
+$(function() {
+    $("a.series_complete").click(quick_series_complete);
+});
