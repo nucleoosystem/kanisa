3944bde30a2cbf079e11becc8c225c1d452f6a37
Dominic Rodger <dominicrodger+github@gmail.com>
2012-08-14 07:59:48 +0100

    Removing another use of fixtures.

--- kanisa/tests/public_views.py
+++ kanisa/tests/public_views.py
@@ -1,11 +1,25 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from kanisa.models import Banner
+import factory
+
+
+class BannerFactory(factory.Factory):
+    FACTORY_FOR = Banner
+    headline = factory.Sequence(lambda n: 'Banner Title #' + n)
+    contents = 'Banner contents'
+    image = 'non_existent_image.jpg'
 
 
 class PublicViewTest(TestCase):
-    fixtures = ['banners.json', ]
-
     def test_kanisa_root_view(self):
+        banner1 = BannerFactory.create()
+        banner2 = BannerFactory.create()
+        banner3 = BannerFactory.create()
+        banner4 = BannerFactory.create()
+        banner5 = BannerFactory.create()
+        banner4.set_retired()
+
         url = reverse('kanisa.views.index')
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
@@ -13,4 +27,4 @@ class PublicViewTest(TestCase):
 
         self.assertTrue('banners' in resp.context)
         self.assertEqual([banner.pk for banner in resp.context['banners']],
-                         [1, 2, 3, 5, ])
+                         [banner1.pk, banner2.pk, banner3.pk, banner5.pk, ])
