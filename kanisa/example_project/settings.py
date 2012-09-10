0d2c47de6e50fc269e8b19893caa9c7ac1c64505
Dominic Rodger <dominicrodger+github@gmail.com>
2012-09-10 19:41:16 +0100

    Fixing typo.

--- kanisa/example_project/settings.py
+++ kanisa/example_project/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'kanisa.middle.KanisaPageFallbackMiddleware',
+    'kanisa.middleware.KanisaPageFallbackMiddleware',
 )
 
 DATE_FORMAT = 'j M Y'
