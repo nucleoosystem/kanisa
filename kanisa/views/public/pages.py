f4fa3ad98e786659e894abc2f37deb15a1000dc9
Dominic Rodger <dominicrodger+github@gmail.com>
2012-09-12 17:23:15 +0100

    Fix for uncaught exception in public page view.

--- kanisa/views/public/pages.py
+++ kanisa/views/public/pages.py
@@ -8,12 +8,16 @@ from kanisa.models.pages import Page, get_page_from_path
 def public_page_view(request):
     try:
         page = get_page_from_path(request.path)
+
         return render_to_response('kanisa/public/pages/page.html',
                                   {'page': page},
                                   context_instance=RequestContext(request))
     except Page.DoesNotExist:
         if not request.path.endswith('/') and settings.APPEND_SLASH:
-            page = get_page_from_path(request.path + '/')
+            try:
+                page = get_page_from_path(request.path + '/')
+            except Page.DoesNotExist:
+                raise Http404
             return HttpResponsePermanentRedirect('%s/' % request.path)
         else:
             raise Http404
