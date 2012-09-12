ea5fd8ea38dca37dbb096e2c7bfb53b89c809dba
Dominic Rodger <dominicrodger+github@gmail.com>
2012-09-12 08:18:25 +0100

    Move the view part of the pages stuff to a function.

--- kanisa/middleware.py
+++ kanisa/middleware.py
@@ -1,38 +1,22 @@
-from django.http import Http404, HttpResponsePermanentRedirect
+from django.http import Http404
 from django.conf import settings
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from kanisa.models.pages import Page, get_page_from_path
+from kanisa.views.public.pages import public_page_view
 
 
 class KanisaPageFallbackMiddleware(object):
-    def get_page_response(self, request):
-        try:
-            page = get_page_from_path(request.path)
-            return render_to_response('kanisa/public/pages/page.html',
-                                      {'page': page},
-                                      context_instance=RequestContext(request))
-        except Page.DoesNotExist:
-            if not request.path.endswith('/') and settings.APPEND_SLASH:
-                page = get_page_from_path(request.path + '/')
-                return HttpResponsePermanentRedirect('%s/' % request.path)
-            else:
-                raise
-
     def process_response(self, request, response):
         if response.status_code != 404:
             # No need to check for a page for non-404 responses.
             return response
+
         try:
-            return self.get_page_response(request)
-        except Page.DoesNotExist:
+            return public_page_view(request)
+        except Http404:
             return response
 
         # Return the original response if any errors happened. Because
         # this is a middleware, we can't assume the errors will be
         # caught elsewhere.
-        except Http404:
-            return response
         except:
             if settings.DEBUG:
                 raise
---
+++ kanisa/views/public/pages.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.http import Http404, HttpResponsePermanentRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from kanisa.models.pages import Page, get_page_from_path
+
+
+def public_page_view(request):
+    try:
+        page = get_page_from_path(request.path)
+        return render_to_response('kanisa/public/pages/page.html',
+                                  {'page': page},
+                                  context_instance=RequestContext(request))
+    except Page.DoesNotExist:
+        if not request.path.endswith('/') and settings.APPEND_SLASH:
+            page = get_page_from_path(request.path + '/')
+            return HttpResponsePermanentRedirect('%s/' % request.path)
+        else:
+            raise Http404
