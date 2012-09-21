8f78c6eb8a375d93120424bc27503950dbcdd61b
Dominic Rodger <dominicrodger+github@gmail.com>
2012-09-21 08:03:17 +0100

    Move pages into its own admin module.

--- kanisa/admin/__init__.py
+++ kanisa/admin/__init__.py
@@ -1,16 +1,5 @@
-from django.contrib import admin
-from kanisa.admin.base import KanisaBaseAdmin
-from kanisa.models import Page
-from mptt.admin import MPTTModelAdmin
-from sorl.thumbnail.admin import AdminImageMixin
-
 import kanisa.admin.banners
 import kanisa.admin.diary
 import kanisa.admin.documents
 import kanisa.admin.sermons
-
-
-class PageAdmin(MPTTModelAdmin):
-    pass
-
-admin.site.register(Page, PageAdmin)
+import kanisa.admin.pages
---
+++ kanisa/admin/pages.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from kanisa.models import Page
+from mptt.admin import MPTTModelAdmin
+
+class PageAdmin(MPTTModelAdmin):
+    pass
+
+admin.site.register(Page, PageAdmin)
