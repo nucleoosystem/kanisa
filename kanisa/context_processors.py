e857cac55592a7d0add75fb5f7c51a3746f59445
Dominic Rodger <dominicrodger+github@gmail.com>
2012-09-21 19:38:15 +0100

    Make sure we cache child nodes too.

--- kanisa/context_processors.py
+++ kanisa/context_processors.py
@@ -7,7 +7,8 @@ def kanisa_settings(context):
     elements = cache.get('kanisa_navigation')
 
     if not elements:
-        elements = NavigationElement.objects.filter(parent=None)
+        root_elements = NavigationElement.objects.filter(parent=None)
+        elements = [(r, r.children.all()) for r in root_elements]
         cache.set('kanisa_navigation', elements)
 
     return {
