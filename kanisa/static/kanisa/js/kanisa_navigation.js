d8d10d6851e1e01733712baf026ded8f03eb01f6
Dominic Rodger <dominicrodger+github@gmail.com>
2012-09-23 22:26:54 +0100

    Allow removing navigation elements via XHR.

--- kanisa/static/kanisa/js/kanisa_navigation.js
+++ kanisa/static/kanisa/js/kanisa_navigation.js
@@ -26,6 +26,7 @@ function navigation_move_down(event) {
 function bind_move_handlers() {
     $("a.move_up").click(navigation_move_up);
     $("a.move_down").click(navigation_move_down);
+    bind_delete_handlers();
 }
 
 function update_navigation_list() {
@@ -34,6 +35,21 @@ function update_navigation_list() {
               $("#navigation_details_container").html(data);
               bind_move_handlers();
           });
+}
+
+function quick_navigation_delete(event) {
+    event.preventDefault();
+    title = $(this).attr("data-navigation-title");
+    if (confirm("Are you sure you want to delete the navigation element \"" + title + "\"?")) {
+        $.post($(this).attr("href"),
+               function(data) {
+                   update_navigation_list();
+               });
+    }
+}
+
+function bind_delete_handlers() {
+    $("a.navigation_delete").click(quick_navigation_delete);
 }
 
 $(function() {
