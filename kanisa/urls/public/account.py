9ac3a9114aaa4a189b866877f84869c967d1c8ea
Dominic Rodger <dominicrodger+github@gmail.com>
2012-09-23 23:00:01 +0100

    Fixes #6 - adding view for changing passwords.

--- kanisa/urls/public/account.py
+++ kanisa/urls/public/account.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.views import password_change
+from kanisa.forms.account import KanisaChangePasswordForm
 from kanisa.views import KanisaLoginView
 
 
@@ -11,4 +13,11 @@ urlpatterns = patterns('',
                            'django.contrib.auth.views.logout',
                            {'template_name': 'kanisa/logout.html', },
                            'kanisa_public_logout'),
+                       url(r'^password/$',
+                           password_change,
+                           {'template_name': ('kanisa/management/'
+                                              'password_reset.html'),
+                            'post_change_redirect': '/manage/',
+                            'password_change_form': KanisaChangePasswordForm},
+                           'kanisa_password_change'),
                        )
