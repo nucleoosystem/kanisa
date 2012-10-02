a9cb23867aa17bcae7403c12be1b38c67c25fd11
Dominic Rodger <dominicrodger+github@gmail.com>
2012-10-02 18:02:58 +0100

    Fixes #19 - logged-in users can see draft pages.

--- kanisa/views/public/pages.py
+++ kanisa/views/public/pages.py
@@ -2,12 +2,19 @@ from django.conf import settings
 from django.http import Http404, HttpResponsePermanentRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from kanisa.models.pages import Page, get_page_from_path
+from kanisa.models.pages import (Page,
+                                 get_page_from_path,
+                                 get_page_from_path_including_drafts)
 
 
 def public_page_view(request):
+    if request.user.has_perm('kanisa.manage_pages'):
+        func = get_page_from_path_including_drafts
+    else:
+        func = get_page_from_path
+
     try:
-        page = get_page_from_path(request.path)
+        page = func(request.path)
 
         return render_to_response('kanisa/public/pages/page.html',
                                   {'page': page,
@@ -16,7 +23,7 @@ def public_page_view(request):
     except Page.DoesNotExist:
         if not request.path.endswith('/') and settings.APPEND_SLASH:
             try:
-                page = get_page_from_path(request.path + '/')
+                page = func(request.path + '/')
             except Page.DoesNotExist:
                 raise Http404
             return HttpResponsePermanentRedirect('%s/' % request.path)
