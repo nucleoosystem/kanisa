3ef98436271c0d13357c8a8b58f1e57dbe350f50
Dominic Rodger <dominicrodger+github@gmail.com>
2012-10-07 20:44:26 +0100

    Split off user JS into its own file.

--- kanisa/static/kanisa/js/kanisa.js
+++ kanisa/static/kanisa/js/kanisa.js
@@ -63,39 +63,4 @@ $(function() {
       }).mouseout(function(){
         $(".noautoschedule").fadeTo('fast', 1.0);
     });
-
-    $(".kanisa_user_perm").change(function() {
-        checkbox = $(this);
-
-        perm_id = checkbox.attr("data-permission-id");
-        user_id = checkbox.attr("data-user-id");
-        assigned = checkbox.attr("checked") == "checked";
-        spinner_id = "spinner-" + perm_id.replace(".", "_");
-
-        checkbox.hide();
-
-        checkbox.after("<i class=\"spinner\" id=\"" + spinner_id + "\"></i>");
-        spinner = $("#" + spinner_id);
-
-        $.post(permission_change_url,
-               { 'permission': perm_id,
-                 'user': user_id,
-                 'assigned': assigned },
-               function(data) {
-                   spinner.remove();
-                   checkbox.show();
-               }).error(function(data) {
-                   alert_failure(data.responseText);
-
-                   if (assigned) {
-                       checkbox.removeAttr("checked");
-                   }
-                   else {
-                       checkbox.attr("checked", "checked");
-                   }
-
-                   spinner.remove();
-                   checkbox.show();
-               });
-    });
 });
---
+++ kanisa/static/kanisa/js/kanisa_users.js
@@ -0,0 +1,36 @@
+$(function() {
+    $(".kanisa_user_perm").change(function() {
+        checkbox = $(this);
+
+        perm_id = checkbox.attr("data-permission-id");
+        user_id = checkbox.attr("data-user-id");
+        assigned = checkbox.attr("checked") == "checked";
+        spinner_id = "spinner-" + perm_id.replace(".", "_");
+
+        checkbox.hide();
+
+        checkbox.after("<i class=\"spinner\" id=\"" + spinner_id + "\"></i>");
+        spinner = $("#" + spinner_id);
+
+        $.post(permission_change_url,
+               { 'permission': perm_id,
+                 'user': user_id,
+                 'assigned': assigned },
+               function(data) {
+                   spinner.remove();
+                   checkbox.show();
+               }).error(function(data) {
+                   alert_failure(data.responseText);
+
+                   if (assigned) {
+                       checkbox.removeAttr("checked");
+                   }
+                   else {
+                       checkbox.attr("checked", "checked");
+                   }
+
+                   spinner.remove();
+                   checkbox.show();
+               });
+    });
+});
