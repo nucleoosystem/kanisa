cad6d4c2af18024329704c88d6870b4e9325e4e4
Dominic Rodger <dominicrodger+github@gmail.com>
2012-10-07 20:56:19 +0100

    Whitespace tidy.

--- kanisa/static/kanisa/js/kanisa_users.js
+++ kanisa/static/kanisa/js/kanisa_users.js
@@ -1,36 +1,36 @@
 function on_user_checkbox_change(event) {
-        checkbox = $(this);
+    checkbox = $(this);
 
-        perm_id = checkbox.attr("data-permission-id");
-        user_id = checkbox.attr("data-user-id");
-        assigned = checkbox.attr("checked") == "checked";
-        spinner_id = "spinner-" + perm_id.replace(".", "_");
+    perm_id = checkbox.attr("data-permission-id");
+    user_id = checkbox.attr("data-user-id");
+    assigned = checkbox.attr("checked") == "checked";
+    spinner_id = "spinner-" + perm_id.replace(".", "_");
 
-        checkbox.hide();
+    checkbox.hide();
 
-        checkbox.after("<i class=\"spinner\" id=\"" + spinner_id + "\"></i>");
-        spinner = $("#" + spinner_id);
+    checkbox.after("<i class=\"spinner\" id=\"" + spinner_id + "\"></i>");
+    spinner = $("#" + spinner_id);
 
-        $.post(permission_change_url,
-               { 'permission': perm_id,
-                 'user': user_id,
-                 'assigned': assigned },
-               function(data) {
-                   spinner.remove();
-                   checkbox.show();
-               }).error(function(data) {
-                   alert_failure(data.responseText);
+    $.post(permission_change_url,
+           { 'permission': perm_id,
+             'user': user_id,
+             'assigned': assigned },
+           function(data) {
+               spinner.remove();
+               checkbox.show();
+           }).error(function(data) {
+               alert_failure(data.responseText);
 
-                   if (assigned) {
-                       checkbox.removeAttr("checked");
-                   }
-                   else {
-                       checkbox.attr("checked", "checked");
-                   }
+               if (assigned) {
+                   checkbox.removeAttr("checked");
+               }
+               else {
+                   checkbox.attr("checked", "checked");
+               }
 
-                   spinner.remove();
-                   checkbox.show();
-               });
+               spinner.remove();
+               checkbox.show();
+           });
 }
 
 $(function() {
