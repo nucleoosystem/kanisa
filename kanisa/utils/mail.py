f5b2b788c5fff229ccf666201a1f6aca9edde473
Dominic Rodger <dominicrodger+github@gmail.com>
2012-10-19 18:49:37 +0100

    Replacing get_template and render with render_to_string.

--- kanisa/utils/mail.py
+++ kanisa/utils/mail.py
@@ -1,22 +1,16 @@
 from django.core.mail import EmailMultiAlternatives
-from django.template import Context
-from django.template.loader import get_template
+from django.template.loader import render_to_string
 from kanisa import conf
 
 
-def send_bulk_mail(users, template, ctx):
-    ctx.update({'KANISA_CHURCH_NAME': conf.KANISA_CHURCH_NAME})
+def send_bulk_mail(users, template, context):
+    context.update({'KANISA_CHURCH_NAME': conf.KANISA_CHURCH_NAME})
 
-    context = Context(ctx)
     template_root = 'kanisa/emails/' + template
 
-    subject_tmpl = get_template(template_root + ".subj")
-    plaintext_tmpl = get_template(template_root + ".txt")
-    html_tmpl = get_template(template_root + ".html")
-
-    subject = subject_tmpl.render(context).strip()
-    plaintext = plaintext_tmpl.render(context)
-    html = html_tmpl.render(context)
+    subject = render_to_string(template_root + ".subj", context).strip()
+    plaintext = render_to_string(template_root + ".txt", context)
+    html = render_to_string(template_root + ".html", context)
 
     for u in users:
         msg = EmailMultiAlternatives(subject,
