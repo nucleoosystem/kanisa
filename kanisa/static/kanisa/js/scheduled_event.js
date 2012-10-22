255280774198ee57dc49ba8f698bc4a35498353a
Dominic Rodger <dominicrodger+github@gmail.com>
2012-10-22 22:25:19 +0100

    Clean up the scheduled event JS - now no global variables.

--- kanisa/static/kanisa/js/scheduled_event.js
+++ kanisa/static/kanisa/js/scheduled_event.js
@@ -1,23 +1,30 @@
+function on_event_type_selection_change() {
+    // This function sets the event title to the name of the selected
+    // event type, every time the event type has changed, unless the
+    // event title has been manually edited.
+
+    var selection = $(this).find(":selected").text();
+
+    if ($(this).val().length == 0) {
+        // Presumably we've selected the default (null) option,
+        // let's reset to the empty string (don't want a title
+        // full of dashes).
+        selection = '';
+    }
+
+    var title_input = $("#id_title");
+
+    // Don't overwrite things the user has manually changed.
+    var last_automatic_value = title_input.attr("data-last-automatic-value");
+
+    if (typeof last_automatic_value != "undefined" &&  title_input.val() != last_automatic_value) {
+        return;
+    }
+
+    title_input.val(selection);
+    title_input.attr("data-last-automatic-value", selection);
+}
+
 $(function() {
-    var last_automatic_event_title = ''
-
-    $("#id_event").change(function() {
-        var selection = $(this).find(":selected").text();
-
-        if ($(this).val().length == 0) {
-            // Presumably we've selected the default (null) option,
-            // let's reset to the empty string (don't want a title
-            // full of dashes).
-            selection = '';
-        }
-
-        var title_input = $("#id_title");
-
-        if (title_input.val() != last_automatic_event_title) {
-            return;
-        }
-
-        title_input.val(selection);
-        last_automatic_event_title = selection;
-    });
+    $("#id_event").change(on_event_type_selection_change);
 });
