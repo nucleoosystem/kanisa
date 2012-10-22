b9559a30366733493268ce947d739a6827660bee
Dominic Rodger <dominicrodger+github@gmail.com>
2012-10-22 17:28:11 +0100

    Break out diary URLs.

---
+++ kanisa/urls/management/xhr/diary.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from kanisa.views.xhr.diary import (ScheduleRegularEventView,
+                                    DiaryGetSchedule)
+
+
+urlpatterns = patterns('',
+                       url(r'^schedule/update/$',
+                           ScheduleRegularEventView.as_view(),
+                           {},
+                           'kanisa_manage_xhr_diary_schedule_regular'),
+                       url(r'^schedule/fetch/(?P<date>\d+)/$',
+                           DiaryGetSchedule.as_view(),
+                           {},
+                           'kanisa_manage_xhr_diary_get_schedule'),
+                       )
