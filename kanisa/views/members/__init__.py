713187e127b5fa44e152809fa41789224692c79a
Dominic Rodger <dominicrodger+github@gmail.com>
2012-10-30 17:39:56 +0000

    Factoring out members' authorisation to a base class.

--- kanisa/views/members/__init__.py
+++ kanisa/views/members/__init__.py
@@ -2,9 +2,11 @@ from django.views.generic.base import TemplateView
 from kanisa.views.generic import KanisaAuthorizationMixin
 
 
-class MembersIndexView(KanisaAuthorizationMixin, TemplateView):
-    template_name = 'kanisa/members/index.html'
-
+class MembersBaseView(KanisaAuthorizationMixin):
     def authorization_check(self, user):
         return user.is_active
+
+
+class MembersIndexView(MembersBaseView, TemplateView):
+    template_name = 'kanisa/members/index.html'
 index = MembersIndexView.as_view()
