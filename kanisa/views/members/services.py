1ae0549537db8c82b249eef956c060afeaff7223
Dominic Rodger <dominicrodger+github@gmail.com>
2012-11-06 23:09:03 +0000

    Add link to toggle between all plans/future plans only.

--- kanisa/views/members/services.py
+++ kanisa/views/members/services.py
@@ -9,10 +9,17 @@ class MembersServicesView(MembersBaseView, KanisaListView):
 
     def get_queryset(self):
         show_all_arg = self.request.GET.get('all', 0)
-        show_all = show_all_arg == '1'
-        if show_all:
+        self.show_all = show_all_arg == '1'
+
+        if self.show_all:
             return Service.objects.all()
         else:
             return Service.future_objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(MembersServicesView,
+                        self).get_context_data(**kwargs)
+        context['showing_all'] = self.show_all
+        return context
 
 index = MembersServicesView.as_view()
