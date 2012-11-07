264fe8b405ac034ca0153100700eb7a8413f6283
Dominic Rodger <dominicrodger+github@gmail.com>
2012-11-07 21:26:51 +0000

    Don't need service field - can get that from the URL.

--- kanisa/forms/services.py
+++ kanisa/forms/services.py
@@ -5,5 +5,4 @@ from kanisa.models import Service, Song
 
 class AddSongToServiceForm(KanisaBaseForm):
     song = forms.ModelChoiceField(queryset=Song.objects.all())
-    service = forms.ModelChoiceField(queryset=Service.objects.all())
     submit_text = 'Add Song'
