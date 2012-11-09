7f888439af11cad66145ebb030b6c76e0c973b17
Dominic Rodger <dominicrodger+github@gmail.com>
2012-11-09 08:23:34 +0000

    Make creating/editing services easier.
    Now we display the event date and the band leader's name.

--- kanisa/forms/services.py
+++ kanisa/forms/services.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.utils import formats
 from kanisa.forms import KanisaBaseForm, KanisaBaseModelForm
-from kanisa.models import Song, Service
+from kanisa.models import Song, Service, ScheduledEvent
 
 
 class AddSongToServiceForm(KanisaBaseForm):
@@ -8,7 +10,22 @@ class AddSongToServiceForm(KanisaBaseForm):
     submit_text = 'Add Song'
 
 
+class EventChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        event_date = formats.date_format(obj.date, "DATE_FORMAT")
+        return '%s (%s)' % (unicode(obj), event_date)
+
+
+class UserChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        full_name = obj.get_full_name()
+        return full_name or obj.username
+
+
 class ServiceForm(KanisaBaseModelForm):
+    event = EventChoiceField(ScheduledEvent.objects.all())
+    band_leader = UserChoiceField(User.objects.all())
+
     class Meta:
         model = Service
         exclude = ('songs', )
