2e1c301b09c7a18965b6d81be83dedcaccd2e799
Dominic Rodger <dominicrodger+github@gmail.com>
2013-11-12 07:09:05 +0000

    Make templates for branding components
    Saves having lots of text in the Python module

--- kanisa/templatetags/kanisa_branding.py
+++ kanisa/templatetags/kanisa_branding.py
@@ -12,34 +12,16 @@ COMPONENTS = {
         'filename': 'logo.jpg',
         'verbose_name': 'Site Logo',
         'sizes': ['480x140', ],
-        'notes': (
-            'Your site logo is used at the top of every page. It should be '
-            'exactly 140px high, and no wider than 500px.'
-        )
     },
     'apple': {
         'filename': 'apple.jpg',
         'verbose_name': 'Apple Icons',
         'sizes': ['144x144', '114x114', '72x72', '57x57', ],
-        'notes': (
-            'These icons are used where people add your site to their home '
-            'screens. The Apple icon needs to be 144px by 144px. This will '
-            'be scaled down for non-retina iPads to 72px by 72px (and for '
-            'iPhones to 114px and 57px respectively). Images that are '
-            'uploaded which are larger than 144px by 144px will be scaled '
-            'down as necessary.'
-        )
     },
     'favicon':  {
         'filename': 'favicon.ico',
         'verbose_name': 'Site Favicon',
         'sizes': ['32x32', ],
-        'notes': (
-            'A favicon is used when adding sites to favourites, and in '
-            'most browsers\' tabs, to allow people to quickly identify '
-            'the tab they\'re after. Uploaded favicons should be '
-            'exactly 32px by 32px.'
-        )
     },
     'colours':  {
         'filename': 'colours.css',
@@ -55,8 +37,9 @@ class BrandingInformation(object):
 
         self.component = component
         self.url = self.get_cached_url()
-        self.notes = COMPONENTS[self.component].get('notes', None)
         self.sizes = COMPONENTS[self.component].get('sizes', [])
+        template_prefix = 'kanisa/management/branding/notes/'
+        self.template_name = '%s/_%s.html' % (template_prefix, component)
         self.verbose_name = COMPONENTS[self.component]['verbose_name']
 
     def get_cached_url(self):
