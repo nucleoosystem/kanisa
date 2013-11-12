506aed8608354b739a88b56acb67fd7db20352d0
Dominic Rodger <dominicrodger+github@gmail.com>
2013-11-12 07:18:39 +0000

    Loosening restrictions on resource names

--- kanisa/urls/management/branding.py
+++ kanisa/urls/management/branding.py
@@ -5,7 +5,7 @@ import kanisa.views.branding as views
 urlpatterns = patterns(
     '',
     url(r'^$', views.branding_management, {}, 'kanisa_manage_branding'),
-    url(r'^images/(?P<resource>[a-z0-9-]+)/$', views.branding_update, {},
+    url(r'^images/(?P<resource>[a-z0-9-_]+)/$', views.branding_update, {},
         'kanisa_manage_branding_logo'),
     url(r'^colours/$', views.branding_update_colours, {},
         'kanisa_manage_branding_colours'),
