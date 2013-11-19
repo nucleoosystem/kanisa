21693aed93e03c2fc427131ece52e56ba7b97b0d
Dominic Rodger <dominicrodger+github@gmail.com>
2013-11-19 08:14:39 +0000

    URLs should have trailing slashes

--- kanisa/urls/management/users.py
+++ kanisa/urls/management/users.py
@@ -5,6 +5,6 @@ import kanisa.views.users as views
 urlpatterns = patterns(
     '',
     url(r'^$', views.user_management, {}, 'kanisa_manage_users'),
-    url(r'^activate/(?P<user_id>\d+)$', views.user_activate, {},
+    url(r'^activate/(?P<user_id>\d+)/$', views.user_activate, {},
         'kanisa_manage_users_activate'),
 )
