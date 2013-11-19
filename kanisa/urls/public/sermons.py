f14785888c531cb9aa218beaed949a47d5012f66
Dominic Rodger <dominicrodger+github@gmail.com>
2013-11-19 08:19:44 +0000

    Give the MP3 download URL a .mp3 extension
    Hopefully that'll make it more obvious what will happen if
    you visit that URL (mostly for robots).

--- kanisa/urls/public/sermons.py
+++ kanisa/urls/public/sermons.py
@@ -13,7 +13,7 @@ urlpatterns = patterns(
     url(r'^$', SermonIndexView.as_view(), {}, 'kanisa_public_sermon_index'),
     url(r'^archive/$', SermonArchiveView.as_view(), {},
         'kanisa_public_sermon_archive'),
-    url(r'^download/(?P<sermon_id>[0-9]+)/$',
+    url(r'^download/(?P<sermon_id>[0-9]+).mp3$',
         SermonDownloadView.as_view(), {}, 'kanisa_public_sermon_download'),
     url(r'^(?P<slug>[a-z0-9-]+)/$', SermonSeriesDetailView.as_view(), {},
         'kanisa_public_sermon_series_detail'),
