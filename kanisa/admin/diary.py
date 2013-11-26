238fe0af056517a9607aaa9c0770fcb8da22beec
Dominic Rodger <dominicrodger+github@gmail.com>
2013-11-26 18:14:22 +0000

    Tidy up imports

--- kanisa/admin/diary.py
+++ kanisa/admin/diary.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from kanisa.admin.base import KanisaBaseAdmin
-from kanisa.models import (EventContact,
-                           RegularEvent,
-                           ScheduledEvent,
-                           EventCategory)
+from kanisa.models import (
+    EventContact,
+    RegularEvent,
+    ScheduledEvent,
+    EventCategory
+)
 from sorl.thumbnail.admin import AdminImageMixin
 
 
