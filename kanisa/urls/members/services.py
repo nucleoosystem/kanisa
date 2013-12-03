13b3035f4392fe4e935c7265b493db0e5ca36eba
Dominic Rodger <dominicrodger+github@gmail.com>
2013-12-03 18:20:06 +0000

    Hopefully make it clear which URLs need moving

--- kanisa/urls/members/services.py
+++ kanisa/urls/members/services.py
@@ -4,16 +4,19 @@ import kanisa.views.members.services as views
 
 urlpatterns = patterns(
     '',
+    # URLs for members
     url(r'^$', views.index, {'show_all': False},
         'kanisa_members_services_index'),
     url(r'^all/$', views.index, {'show_all': True},
         'kanisa_members_services_index_all'),
+    url(r'^service/(?P<service_pk>\d+)/$', views.service_detail, {},
+        'kanisa_members_services_detail'),
+
+    # URLs for management - these need to be moved
     url(r'^service/create/$', views.service_create, {},
         'kanisa_members_services_create'),
     url(r'^service/(?P<service_pk>\d+)/edit/$', views.service_update, {},
         'kanisa_members_services_update'),
-    url(r'^service/(?P<service_pk>\d+)/$', views.service_detail, {},
-        'kanisa_members_services_detail'),
     url(r'^service/(?P<service_pk>\d+)/addsong/$', views.add_song, {},
         'kanisa_members_services_add_song'),
     url(r'^service/(?P<service_pk>\d+)/createsong/$', views.create_song, {},
