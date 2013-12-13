c45e2289eeb67648036e5e7587583ab5385b2262
Dominic Rodger <dominicrodger+github@gmail.com>
2013-12-13 17:26:32 +0000

    Making it possible to create users again
    You need to override forms, to specify the custom user
    model, otherwise all kinds of sadness happen (no such table
    auth_user, mostly).
    
    Also - we don't have first name, last name and email on the
    original creation form, so let's scope those changes to only
    when we have a user instance

--- kanisa/admin/account.py
+++ kanisa/admin/account.py
@@ -1,12 +1,43 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import (
+    UserChangeForm,
+    UserCreationForm,
+)
 from django.utils.translation import ugettext_lazy as _
 from kanisa.conf import KANISA_ADMIN_THUMBS_SIZE
 from kanisa.models import RegisteredUser
 from sorl.thumbnail import default
 
 
+class RegisteredUserCreationForm(UserCreationForm):
+    class Meta:
+        model = RegisteredUser
+        fields = ("username",)
+
+    def clean_username(self):
+        # See django-users/kOVEy9znYn5c
+        username = self.cleaned_data['username']
+        try:
+            self._meta.model._default_manager.get(username=username)
+        except self._meta.model.DoesNotExist:
+            return username
+
+        raise forms.ValidationError(
+            self.error_messages['duplicate_username']
+        )
+
+
+class RegisteredUserChangeForm(UserChangeForm):
+    class Meta:
+        model = RegisteredUser
+        fields = '__all__'
+
+
 class RegisteredUserAdmin(UserAdmin):
+    form = RegisteredUserChangeForm
+    add_form = RegisteredUserCreationForm
+
     list_display = (
         'username',
         'image_thumb',
@@ -35,9 +66,10 @@ class RegisteredUserAdmin(UserAdmin):
         form = super(RegisteredUserAdmin,
                      self).get_form(request, obj, **kwargs)
 
-        form.base_fields['first_name'].required = True
-        form.base_fields['last_name'].required = True
-        form.base_fields['email'].required = True
+        if obj is not None:
+            form.base_fields['first_name'].required = True
+            form.base_fields['last_name'].required = True
+            form.base_fields['email'].required = True
 
         return form
 
