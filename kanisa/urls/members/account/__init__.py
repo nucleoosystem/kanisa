456f037184d863c03f7b27317145332cc3934f76
Dominic Rodger <dominicrodger+github@gmail.com>
2013-12-17 08:11:14 +0000

    Move account URLs into the members URL code

---
+++ kanisa/urls/members/account/__init__.py
@@ -0,0 +1,27 @@
+from django.conf.urls import include, patterns, url
+from kanisa.views.public.auth import (
+    KanisaAccountModificationView,
+    KanisaLoginView,
+    KanisaRegistrationView,
+    KanisaRegistrationThanksView
+)
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^login/$', KanisaLoginView.as_view(), {},
+        'kanisa_members_login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'template_name': 'kanisa/auth/logout.html', },
+        'kanisa_members_logout'),
+
+    url(r'^modify/$', KanisaAccountModificationView.as_view(), {},
+        'kanisa_members_account_modify'),
+
+    url(r'^registration/$', KanisaRegistrationView.as_view(), {},
+        'kanisa_members_registration'),
+    url(r'^registration/thanks/$', KanisaRegistrationThanksView.as_view(), {},
+        'kanisa_members_registration_thanks'),
+
+    url(r'^password/', include('kanisa.urls.members.account.password')),
+)
