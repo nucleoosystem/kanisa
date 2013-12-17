c9fcf291b79ce5312a36f0ea88793422f0771c3d
Dominic Rodger <dominicrodger+github@gmail.com>
2013-12-17 08:09:13 +0000

    Moving password resetting URLs into members/account

--- kanisa/urls/public/account.py
+++ kanisa/urls/public/account.py
@@ -23,5 +23,5 @@ urlpatterns = patterns(
     url(r'^registration/thanks/$', KanisaRegistrationThanksView.as_view(), {},
         'kanisa_members_registration_thanks'),
 
-    url(r'^password/', include('kanisa.urls.public.password')),
+    url(r'^password/', include('kanisa.urls.members.account.password')),
 )
