11aa18117135725c37ec6501bfb22a632a5565cf
Dominic Rodger <dominicrodger+github@gmail.com>
2013-12-17 17:17:52 +0000

    Allow saving a user without a profile picture

--- kanisa/forms/user.py
+++ kanisa/forms/user.py
@@ -21,7 +21,8 @@ class UserUpdateForm(KanisaBaseForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
     email = forms.EmailField()
-    image = forms.FileField(widget=KanisaThumbnailFileWidget(100, 100))
+    image = forms.FileField(widget=KanisaThumbnailFileWidget(100, 100),
+                            required=False)
     permissions = forms.MultipleChoiceField(choices=get_choices(),
                                             label='',
                                             required=False)
