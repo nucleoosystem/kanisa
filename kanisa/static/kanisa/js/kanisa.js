84ebdb13552737461b18e5eb9297bd27958acfaa
Dominic Rodger <dominicrodger+github@gmail.com>
2013-12-25 22:17:28 +0000

    Separating out the alerts JS

--- kanisa/static/kanisa/js/kanisa.js
+++ kanisa/static/kanisa/js/kanisa.js
@@ -39,20 +39,6 @@ jQuery(document).ajaxSend(function(event, xhr, settings) {
 });
 
 // Kanisa-specific functionality
-
-function _alert_impl(msg, alert_class) {
-    alert_box = $("#kanisa_alerts");
-    $(".js-alert").remove();
-    html = "<div class=\"js-alert alert " + alert_class + "\" style=\"display: none\">";
-    html += "<button class=\"close\" data-dismiss=\"alert\">&times;</button>" + msg + "</div>";
-    alert_box.after(html);
-    $(".js-alert").slideDown('fast');
-}
-
-function alert_failure(msg) {
-    _alert_impl(msg, "alert-error");
-}
-
 $(document).ready(function() {
     $("#schedule-weeks-events").mouseover(function() {
         $(".noautoschedule").fadeTo('fast', 0.3);
---
+++ kanisa/static/kanisa/js/kanisa_alerts.js
@@ -0,0 +1,12 @@
+function _alert_impl(msg, alert_class) {
+    alert_box = $("#kanisa_alerts");
+    $(".js-alert").remove();
+    html = "<div class=\"js-alert alert " + alert_class + "\" style=\"display: none\">";
+    html += "<button class=\"close\" data-dismiss=\"alert\">&times;</button>" + msg + "</div>";
+    alert_box.after(html);
+    $(".js-alert").slideDown('fast');
+}
+
+function alert_failure(msg) {
+    _alert_impl(msg, "alert-error");
+}
