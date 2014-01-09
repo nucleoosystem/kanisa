54188f65de021d9c9ca8c96d46241ab710e2888c
Dominic Rodger <dominicrodger+github@gmail.com>
2014-01-09 08:16:30 +0000

    Make assigning categories to events easier

---
+++ kanisa/static/kanisa/js/management/regular_event.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".regulareventform #id_categories").chosen({
+        placeholder_text_multiple: "Select some event categories"
+    });
+    $(".regulareventform #id_categories")
+        .siblings(".chosen-container")
+        .width("100%");
+    $(".regulareventform #hint_id_categories").hide();
+});
