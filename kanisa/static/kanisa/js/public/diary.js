1a36da50e7b50522c57655dba46ef15fd9000018
Dominic Rodger <dominicrodger+github@gmail.com>
2014-01-10 17:43:10 +0000

    Add JS for navigating around the diary

---
+++ kanisa/static/kanisa/js/public/diary.js
@@ -0,0 +1,20 @@
+function bind_previous_week_and_next_week_controls() {
+    $(".week_pager").click(update_this_week_table);
+}
+
+function update_this_week_table(event) {
+    event.preventDefault();
+
+    var el=$(this);
+
+    $.get(update_this_week,
+          {'start_date': el.attr("data-date")},
+          function(data) {
+              $("#this_week_table").html(data);
+              bind_previous_week_and_next_week_controls();
+          });
+}
+
+$(document).ready(function() {
+    bind_previous_week_and_next_week_controls();
+});
