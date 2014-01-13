4c0bb5fca1279685d4c5cbc3d9341d6dad4d0aa7
Dominic Rodger <dominicrodger+github@gmail.com>
2014-01-13 17:53:31 +0000

    Short-circuit for superusers

--- kanisa/utils/auth.py
+++ kanisa/utils/auth.py
@@ -4,8 +4,12 @@ from django.db.models import Q
 
 
 def has_any_kanisa_permission(user):
+    if user.is_superuser:
+        return True
+
     perms = user.get_all_permissions()
     kanisa_management = [p for p in perms if p.startswith('kanisa.manage')]
+
     return len(kanisa_management) > 0
 
 
