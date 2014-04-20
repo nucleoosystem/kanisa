5421fbecdec6061b9b3290e6460fe3821f3049e9
Dominic Rodger <dominicrodger+github@gmail.com>
2014-04-20 21:08:44 +0100

    Splitting up tests so they're a bit easier to follow

--- kanisa/tests/views/test_blog.py
+++ kanisa/tests/views/test_blog.py
@@ -35,24 +35,25 @@ def test_blog_index(rf, posts):
     assert context['object_list'][1] == post2
 
 
+def get_year_view(rf, year):
+    request = rf.get(
+        reverse(
+            'kanisa_public_blog_year',
+            args=[year]
+        )
+    )
+
+    return views.blog_year(
+        request,
+        year='%d' % year
+    )
+
+
 @pytest.mark.django_db
-def test_blog_year_archive(rf, posts):
-    def get(rf, year):
-        request = rf.get(
-            reverse(
-                'kanisa_public_blog_year',
-                args=[year]
-            )
-        )
-
-        return views.blog_year(
-            request,
-            year='%d' % year
-        )
-
+def test_blog_year_archive_recent(rf, posts):
     post1, post2, post3 = posts
 
-    response = get(rf, post1.publish_date.year)
+    response = get_year_view(rf, post1.publish_date.year)
     content = response.rendered_content
     assert len(content) > 0
     assert response.status_code == 200
@@ -62,10 +63,18 @@ def test_blog_year_archive(rf, posts):
     assert len(context['object_list']) == 1
     assert context['object_list'][0] == post1
 
-    with pytest.raises(Http404):
-        get(rf, 2013)
 
-    response = get(rf, 2012)
+@pytest.mark.django_db
+def test_blog_year_archive_empty(rf):
+    with pytest.raises(Http404):
+        get_year_view(rf, 2013)
+
+
+@pytest.mark.django_db
+def test_blog_year_archive_old(rf, posts):
+    post1, post2, post3 = posts
+
+    response = get_year_view(rf, 2012)
     content = response.rendered_content
     assert len(content) > 0
     context = response.context_data
@@ -75,23 +84,24 @@ def test_blog_year_archive(rf, posts):
     assert context['object_list'][0] == post2
 
     with pytest.raises(Http404):
-        get(rf, post3.publish_date.year)
+        get_year_view(rf, post3.publish_date.year)
+
+
+def get_blog_detail(rf, post):
+    request = rf.get(post.get_absolute_url())
+
+    return views.blog_detail(
+        request,
+        year='%d' % post.publish_date.year,
+        slug=post.slug
+    )
 
 
 @pytest.mark.django_db
-def test_blog_detail(rf, posts):
-    def get(rf, post):
-        request = rf.get(post.get_absolute_url())
-
-        return views.blog_detail(
-            request,
-            year='%d' % post.publish_date.year,
-            slug=post.slug
-        )
-
+def test_blog_detail_no_next(rf, posts):
     post1, post2, post3 = posts
 
-    response = get(rf, post1)
+    response = get_blog_detail(rf, post1)
     assert response.status_code == 200
     content = response.rendered_content
     assert len(content) > 0
@@ -100,7 +110,11 @@ def test_blog_detail(rf, posts):
     assert context['next'] is None
     assert context['previous'] == post2
 
-    response = get(rf, post2)
+@pytest.mark.django_db
+def test_blog_detail_no_previous(rf, posts):
+    post1, post2, post3 = posts
+
+    response = get_blog_detail(rf, post2)
     assert response.status_code == 200
     content = response.rendered_content
     assert len(content) > 0
@@ -109,5 +123,9 @@ def test_blog_detail(rf, posts):
     assert context['next'] == post1
     assert context['previous'] is None
 
+@pytest.mark.django_db
+def test_blog_detail_future(rf, posts):
+    post1, post2, post3 = posts
+
     with pytest.raises(Http404):
-        get(rf, post3)
+        get_blog_detail(rf, post3)
