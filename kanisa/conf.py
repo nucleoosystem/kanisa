5b066f9c1bcfd1a52b4af4f7d828b445cc6cfb3f
Dominic Rodger <dominicrodger+github@gmail.com>
2014-04-23 08:17:34 +0100

    Whitespace tidy

--- kanisa/conf.py
+++ kanisa/conf.py
@@ -1,24 +1,38 @@
 from django.conf import settings
 
-KANISA_ADMIN_THUMBS_SIZE = getattr(settings, 'KANISA_ADMIN_THUMBS_SIZE',
-                                   '60x60')
+KANISA_ADMIN_THUMBS_SIZE = getattr(
+    settings,
+    'KANISA_ADMIN_THUMBS_SIZE',
+    '60x60'
+)
 
-KANISA_CHURCH_NAME = getattr(settings,
-                             'KANISA_CHURCH_NAME',
-                             'Your Church')
+KANISA_CHURCH_NAME = getattr(
+    settings,
+    'KANISA_CHURCH_NAME',
+    'Your Church'
+)
 
-KANISA_REGISTRATION_ALLOWED = getattr(settings,
-                                      'KANISA_ALLOW_REGISTRATION',
-                                      True)
+KANISA_REGISTRATION_ALLOWED = getattr(
+    settings,
+    'KANISA_ALLOW_REGISTRATION',
+    True
+)
 
-KANISA_FROM_EMAIL = getattr(settings,
-                            'KANISA_FROM_EMAIL',
-                            settings.DEFAULT_FROM_EMAIL)
+KANISA_FROM_EMAIL = getattr(
+    settings,
+    'KANISA_FROM_EMAIL',
+    settings.DEFAULT_FROM_EMAIL
+)
 
-KANISA_DEBUG_STATIC = getattr(settings,
-                              'KANISA_DEBUG_STATIC',
-                              settings.DEBUG)
+KANISA_DEBUG_STATIC = getattr(
+    settings,
+    'KANISA_DEBUG_STATIC',
+    settings.DEBUG
+)
 
-KANISA_GOOGLE_ANALYTICS_KEY = getattr(settings,
-                                      'KANISA_GOOGLE_ANALYTICS_KEY',
-                                      None)
+KANISA_GOOGLE_ANALYTICS_KEY = getattr(
+    settings,
+    'KANISA_GOOGLE_ANALYTICS_KEY',
+    None
+)
+
