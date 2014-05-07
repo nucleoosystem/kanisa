be9b52caae13b63c3666988a2c586c037f0af158
Dominic Rodger <dominicrodger+github@gmail.com>
2014-05-07 22:08:23 +0100

    Fix JS error on all non-blog pages

--- kanisa/static/kanisa/js/management/kanisa_blog.js
+++ kanisa/static/kanisa/js/management/kanisa_blog.js
@@ -1,5 +1,5 @@
 function get_rough_word_count(str) {
-  return str.split(/\s+/).length;
+    return str.split(/\s+/).length;
 }
 
 function hide_word_count_warning(element) {
@@ -13,6 +13,10 @@ function show_word_count_warning(words, element) {
 }
 
 function update_word_count_impl(element) {
+    if (element.length == 0) {
+        return;
+    }
+
     var word_count = get_rough_word_count(element.val());
     var help_block = element.parent().parent().children(".help-block")
 
