30fc7908cf4fc6d786af3ebcd453d26799559957
Dominic Rodger <dominicrodger+github@gmail.com>
2014-05-09 17:17:18 +0100

    URL tidying

--- kanisa/urls/members/services.py
+++ kanisa/urls/members/services.py
@@ -11,18 +11,18 @@ urlpatterns = patterns(
     url(r'^all/$', views.index, {'show_all': True},
         'kanisa_members_services_index_all'),
 
-    url(r'^service/(?P<service_pk>\d+)/$', views.service_detail, {},
+    url(r'^services/(?P<service_pk>\d+)/$', views.service_detail, {},
         'kanisa_members_services_detail'),
 
     url(r'^songs/$', song_views.song_list, {},
         'kanisa_members_services_songs'),
+    url(r'^songs/composers/(?P<composer_pk>\d+)/$',
+        song_views.composer_detail, {},
+        'kanisa_members_services_composer_detail'),
     url(r'^songs/discover/$', song_views.song_discovery, {},
         'kanisa_members_services_song_discovery'),
     url(r'^songs/(?P<song_pk>\d+)/$', song_views.song_detail, {},
         'kanisa_members_services_song_detail'),
-
-    url(r'^composers/(?P<composer_pk>\d+)/$', song_views.composer_detail, {},
-        'kanisa_members_services_composer_detail'),
 
     url(r'^ccli/$', ccli_views.ccli_view, {},
         'kanisa_members_services_ccli'),
