0a8e532115cbb9bbfa6865d6d847c42ebfe229b1
Dominic Rodger <dominicrodger+github@gmail.com>
2014-05-11 21:00:14 +0100

    Attempt to fix some CSRF problems
    All AJAX post requests are currently being rejected.

---
+++ kanisa/static/kanisa/js/management/csrf.js
@@ -0,0 +1,16 @@
+// Code taken from https://docs.djangoproject.com/en/dev/ref/contrib/csrf/#ajax
+
+var csrftoken = $.cookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    crossDomain: false, // obviates need for sameOrigin test
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type)) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
