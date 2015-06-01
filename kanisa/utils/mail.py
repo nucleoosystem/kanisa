7f05c74c7cf433f5fa205c52717f4ccb06122374
Dominic Rodger <dominicrodger+github@gmail.com>
2015-06-01 22:25:23 +0100

    Factor out mail sending code so we can use it without a user object

--- kanisa/utils/mail.py
+++ kanisa/utils/mail.py
@@ -3,27 +3,33 @@ from django.template.loader import render_to_string
 from kanisa import conf
 
 
+def send_mail_with_context(email, context, template):
+    template_root = 'kanisa/emails/' + template
+
+    subject = render_to_string(template_root + ".subj",
+                               context).strip()
+    plaintext = render_to_string(template_root + ".txt",
+                                 context)
+    html = render_to_string(template_root + ".html",
+                            context)
+
+    msg = EmailMultiAlternatives(
+        subject,
+        plaintext,
+        conf.KANISA_FROM_EMAIL,
+        [email, ]
+    )
+    msg.attach_alternative(html, "text/html")
+    msg.send()
+
+
 def send_bulk_mail(users, template, context):
     context.update({'KANISA_CHURCH_NAME': conf.KANISA_CHURCH_NAME})
 
-    template_root = 'kanisa/emails/' + template
-
-    for u in users:
+    for user in users:
         user_context = context.copy()
-        user_context['addressee'] = u
-        subject = render_to_string(template_root + ".subj",
-                                   user_context).strip()
-        plaintext = render_to_string(template_root + ".txt",
-                                     user_context)
-        html = render_to_string(template_root + ".html",
-                                user_context)
-
-        msg = EmailMultiAlternatives(subject,
-                                     plaintext,
-                                     conf.KANISA_FROM_EMAIL,
-                                     [u.email, ])
-        msg.attach_alternative(html, "text/html")
-        msg.send()
+        user_context['addressee'] = user
+        send_mail_with_context(user.email, user_context, template)
 
 
 def send_single_mail(user, template, context):
