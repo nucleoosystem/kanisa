e229b3806272334a22907e160a5939a2a867b681
Dominic Rodger <dominicrodger+github@gmail.com>
2018-05-25 20:57:18 +0100

    Tidy URLs

--- kanisa/urls/management/documents.py
+++ kanisa/urls/management/documents.py
@@ -6,10 +6,10 @@ urlpatterns = [
     url(r'^$', views.document_management, {}, 'kanisa_manage_documents'),
     url(r'^create/$', views.document_create, {},
         'kanisa_manage_documents_create'),
-    url(r'^edit/(?P<pk>\d+)$', views.document_update, {},
+    url(r'^edit/(?P<pk>\d+)/$', views.document_update, {},
         'kanisa_manage_documents_update'),
-    url(r'^delete/(?P<pk>\d+)$', views.document_delete, {},
+    url(r'^delete/(?P<pk>\d+)/$', views.document_delete, {},
         'kanisa_manage_documents_delete'),
-    url(r'^expire/(?P<pk>\d+)$', views.document_expire, {},
+    url(r'^expire/(?P<pk>\d+)/$', views.document_expire, {},
         'kanisa_manage_documents_expire'),
 ]
