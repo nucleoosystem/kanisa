b2977a8e742bf745c9025c596a8ba6a6ac92b271
Dominic Rodger <dominicrodger+github@gmail.com>
2018-05-25 17:52:46 +0100

    Show newest images first

--- kanisa/views/management/media.py
+++ kanisa/views/management/media.py
@@ -18,12 +18,13 @@ class MediaBaseView(KanisaAuthorizationMixin):
 class MediaIndexView(MediaBaseView,
                      KanisaListView):
     model = InlineImage
-    queryset = InlineImage.objects.all()
+    queryset = InlineImage.objects.all().order_by('-modified')
 
     template_name = 'kanisa/management/media/index.html'
     kanisa_title = 'Manage Media'
     kanisa_is_root_view = True
     paginate_by = 24
+
 media_management = MediaIndexView.as_view()
 
 
