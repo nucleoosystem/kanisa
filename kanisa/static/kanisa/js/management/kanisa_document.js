cc65a7c2ef2914def4352e7301bf70406750c2e6
Dominic Rodger <dominicrodger+github@gmail.com>
2018-05-25 23:06:17 +0100

    Allow expiring documents without page reloads

--- kanisa/static/kanisa/js/management/kanisa_document.js
+++ kanisa/static/kanisa/js/management/kanisa_document.js
@@ -1,14 +1,12 @@
-function lookup_documents(evt) {
-    var el = $(this);
-    var current_val = el.val();
-
+function reload_documents(title) {
     $.get(
         "",
         {
-            'title': current_val,
+            'title': title,
         },
         function(data) {
             $("#document_results").html(data);
+            bind_document_expiry_handlers();
         }
     ).error(
         function(data) {
@@ -17,6 +15,36 @@ function lookup_documents(evt) {
     );
 }
 
+function lookup_documents(evt) {
+    var el = $(this);
+    var current_val = el.val();
+    reload_documents(current_val);
+}
+
+function handle_expiry_toggle(event) {
+    event.preventDefault();
+    var form = $(this);
+
+    if (form.data('submitting') === true) {
+        return false;
+    }
+
+    form.data('submitting', true);
+    $.post(
+        form.attr("action"),
+        {},
+        function(data) {
+            reload_documents($("#id_search").val());
+        }).error(function(data) {
+            form.data('submitting', false);
+        });
+}
+
+function bind_document_expiry_handlers() {
+    $("form.expiry_toggle").submit(handle_expiry_toggle);
+}
+
 $(document).ready(function() {
     $("#document_form #id_search").on('input', lookup_documents);
+    bind_document_expiry_handlers();
 });
